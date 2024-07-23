b81fed41668a29ba31ec60be56500b7cd3bc0533
stuwjn <1375559540@qq.com>
2024-07-23 23:54:57 +0800

    Default theme build

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
+  title: "📺",
   description: "A VitePress Site",
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -26,3 +27,4 @@ export default defineConfig({
     ]
   }
 })
+
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,4 @@
- // https://vitepress.dev/guide/custom-theme
+// https://vitepress.dev/guide/custom-theme
  import { h } from 'vue'
  import type { Theme } from 'vitepress'
  import DefaultTheme from 'vitepress/theme'
@@ -8,18 +8,11 @@
    extends: DefaultTheme,
    Layout: () => {
      return h(DefaultTheme.Layout, null, {
-       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-     })
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
    },
    enhanceApp({ app, router, siteData }) {
      // ...
    }
  } satisfies Theme
 
-// 1. import vitepress theme
-//import Theme from '@escook/vitepress-theme'
-// 2. import matching CSS styles (this step cannot be omitted)
-//import '@escook/vitepress-theme/style.css'
-
-// 3. simply set the theme of "import" to "export default"
-//export default Theme
