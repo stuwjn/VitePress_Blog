7ed4cdb284eaeacaaaa01dde6d70dbb21557f770
stuwjn <1375559540@qq.com>
2024-07-23 20:42:33 +0800

    Restore native theme

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,25 +1,25 @@
-// // https://vitepress.dev/guide/custom-theme
-// import { h } from 'vue'
-// import type { Theme } from 'vitepress'
-// import DefaultTheme from 'vitepress/theme'
-// import './style.css'
+ // https://vitepress.dev/guide/custom-theme
+ import { h } from 'vue'
+ import type { Theme } from 'vitepress'
+ import DefaultTheme from 'vitepress/theme'
+ import './style.css'
 
-// export default {
-//   extends: DefaultTheme,
-//   Layout: () => {
-//     return h(DefaultTheme.Layout, null, {
-//       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-//     })
-//   },
-//   enhanceApp({ app, router, siteData }) {
-//     // ...
-//   }
-// } satisfies Theme
+ export default {
+   extends: DefaultTheme,
+   Layout: () => {
+     return h(DefaultTheme.Layout, null, {
+       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+     })
+   },
+   enhanceApp({ app, router, siteData }) {
+     // ...
+   }
+ } satisfies Theme
 
 // 1. import vitepress theme
-import Theme from '@escook/vitepress-theme'
+//import Theme from '@escook/vitepress-theme'
 // 2. import matching CSS styles (this step cannot be omitted)
-import '@escook/vitepress-theme/style.css'
+//import '@escook/vitepress-theme/style.css'
 
 // 3. simply set the theme of "import" to "export default"
-export default Theme
+//export default Theme
